3cb8661e34b7539c534d0eaf6d5cf92426a377c9
Bradley Voracek <brad@Bradleys-MacBook-Air.local>
2026-09-01 08:50:18 -0700

    Add public need layers to job bank atlas

---
+++ public/job-bank-atlas/needs-data.js
@@ -0,0 +1,371 @@
+globalThis.JOB_BANK_NEEDS = {
+  "vintage": "2024 ACS 1-year estimates",
+  "generatedAt": "2026-09-01T04:55:47.344Z",
+  "source": "https://www.census.gov/programs-surveys/acs/data/summary-file.html",
+  "values": {
+    "10": {
+      "name": "Delaware",
+      "olderAdults": 21.7,
+      "childPoverty": 12.6,
+      "rentBurden": 50.3,
+      "uninsured": 6.9
+    },
+    "11": {
+      "name": "District of Columbia",
+      "olderAdults": 12.9,
+      "childPoverty": 28.3,
+      "rentBurden": 47.7,
+      "uninsured": 4.5
+    },
+    "12": {
+      "name": "Florida",
+      "olderAdults": 21.8,
+      "childPoverty": 15.5,
+      "rentBurden": 62.1,
+      "uninsured": 10.9
+    },
+    "13": {
+      "name": "Georgia",
+      "olderAdults": 15.8,
+      "childPoverty": 16.3,
+      "rentBurden": 53.1,
+      "uninsured": 12
+    },
+    "15": {
+      "name": "Hawaii",
+      "olderAdults": 21.5,
+      "childPoverty": 11.5,
+      "rentBurden": 55,
+      "uninsured": 3.5
+    },
+    "16": {
+      "name": "Idaho",
+      "olderAdults": 17.8,
+      "childPoverty": 11.1,
+      "rentBurden": 48.4,
+      "uninsured": 9.2
+    },
+    "17": {
+      "name": "Illinois",
+      "olderAdults": 17.9,
+      "childPoverty": 14.1,
+      "rentBurden": 48.7,
+      "uninsured": 6.9
+    },
+    "18": {
+      "name": "Indiana",
+      "olderAdults": 17.6,
+      "childPoverty": 16.4,
+      "rentBurden": 48.5,
+      "uninsured": 7.5
+    },
+    "19": {
+      "name": "Iowa",
+      "olderAdults": 18.9,
+      "childPoverty": 13.6,
+      "rentBurden": 43.2,
+      "uninsured": 5.4
+    },
+    "20": {
+      "name": "Kansas",
+      "olderAdults": 18,
+      "childPoverty": 12.8,
+      "rentBurden": 43.4,
+      "uninsured": 8.5
+    },
+    "21": {
+      "name": "Kentucky",
+      "olderAdults": 18,
+      "childPoverty": 19.3,
+      "rentBurden": 45.2,
+      "uninsured": 6.8
+    },
+    "22": {
+      "name": "Louisiana",
+      "olderAdults": 17.8,
+      "childPoverty": 25.2,
+      "rentBurden": 54.1,
+      "uninsured": 7.7
+    },
+    "23": {
+      "name": "Maine",
+      "olderAdults": 23.5,
+      "childPoverty": 12.7,
+      "rentBurden": 50.2,
+      "uninsured": 5.5
+    },
+    "24": {
+      "name": "Maryland",
+      "olderAdults": 17.6,
+      "childPoverty": 10.6,
+      "rentBurden": 51.5,
+      "uninsured": 6.3
+    },
+    "25": {
+      "name": "Massachusetts",
+      "olderAdults": 18.7,
+      "childPoverty": 11.1,
+      "rentBurden": 52.4,
+      "uninsured": 2.8
+    },
+    "26": {
+      "name": "Michigan",
+      "olderAdults": 19.6,
+      "childPoverty": 18,
+      "rentBurden": 51.2,
+      "uninsured": 5.1
+    },
+    "27": {
+      "name": "Minnesota",
+      "olderAdults": 18.2,
+      "childPoverty": 9.5,
+      "rentBurden": 47.7,
+      "uninsured": 5.1
+    },
+    "28": {
+      "name": "Mississippi",
+      "olderAdults": 18.1,
+      "childPoverty": 25.2,
+      "rentBurden": 50.3,
+      "uninsured": 9.7
+    },
+    "29": {
+      "name": "Missouri",
+      "olderAdults": 18.7,
+      "childPoverty": 15.4,
+      "rentBurden": 45.2,
+      "uninsured": 7.7
+    },
+    "30": {
+      "name": "Montana",
+      "olderAdults": 21.2,
+      "childPoverty": 11,
+      "rentBurden": 42.7,
+      "uninsured": 8.8
+    },
+    "31": {
+      "name": "Nebraska",
+      "olderAdults": 17.4,
+      "childPoverty": 12,
+      "rentBurden": 46.5,
+      "uninsured": 7.1
+    },
+    "32": {
+      "name": "Nevada",
+      "olderAdults": 17.6,
+      "childPoverty": 14.6,
+      "rentBurden": 57.6,
+      "uninsured": 11.4
+    },
+    "33": {
+      "name": "New Hampshire",
+      "olderAdults": 21.5,
+      "childPoverty": 6.6,
+      "rentBurden": 49.4,
+      "uninsured": 4.5
+    },
+    "34": {
+      "name": "New Jersey",
+      "olderAdults": 18,
+      "childPoverty": 11.7,
+      "rentBurden": 52.2,
+      "uninsured": 7.7
+    },
+    "35": {
+      "name": "New Mexico",
+      "olderAdults": 20.1,
+      "childPoverty": 21.8,
+      "rentBurden": 48.4,
+      "uninsured": 10.1
+    },
+    "36": {
+      "name": "New York",
+      "olderAdults": 18.9,
+      "childPoverty": 17.9,
+      "rentBurden": 51.5,
+      "uninsured": 5
+    },
+    "37": {
+      "name": "North Carolina",
+      "olderAdults": 17.9,
+      "childPoverty": 16.5,
+      "rentBurden": 51.5,
+      "uninsured": 8.6
+    },
+    "38": {
+      "name": "North Dakota",
+      "olderAdults": 17.6,
+      "childPoverty": 12.8,
+      "rentBurden": 36.1,
+      "uninsured": 6.1
+    },
+    "39": {
+      "name": "Ohio",
+      "olderAdults": 19.1,
+      "childPoverty": 16.5,
+      "rentBurden": 47.7,
+      "uninsured": 6.7
+    },
+    "40": {
+      "name": "Oklahoma",
+      "olderAdults": 16.9,
+      "childPoverty": 18.9,
+      "rentBurden": 46.1,
+      "uninsured": 11.5
+    },
+    "41": {
+      "name": "Oregon",
+      "olderAdults": 19.9,
+      "childPoverty": 13,
+      "rentBurden": 52.3,
+      "uninsured": 5.2
+    },
+    "42": {
+      "name": "Pennsylvania",
+      "olderAdults": 20.4,
+      "childPoverty": 15.3,
+      "rentBurden": 49.4,
+      "uninsured": 5.8
+    },
+    "44": {
+      "name": "Rhode Island",
+      "olderAdults": 19.8,
+      "childPoverty": 16.3,
+      "rentBurden": 51.1,
+      "uninsured": 4.6
+    },
+    "45": {
+      "name": "South Carolina",
+      "olderAdults": 19.8,
+      "childPoverty": 18.3,
+      "rentBurden": 52.6,
+      "uninsured": 9
+    },
+    "46": {
+      "name": "South Dakota",
+      "olderAdults": 19,
+      "childPoverty": 11.2,
+      "rentBurden": 41.9,
+      "uninsured": 8.1
+    },
+    "47": {
+      "name": "Tennessee",
+      "olderAdults": 17.6,
+      "childPoverty": 19.1,
+      "rentBurden": 50.2,
+      "uninsured": 9.7
+    },
+    "48": {
+      "name": "Texas",
+      "olderAdults": 14,
+      "childPoverty": 17.9,
+      "rentBurden": 52.7,
+      "uninsured": 16.7
+    },
+    "49": {
+      "name": "Utah",
+      "olderAdults": 12.4,
+      "childPoverty": 8.6,
+      "rentBurden": 48.2,
+      "uninsured": 8.3
+    },
+    "50": {
+      "name": "Vermont",
+      "olderAdults": 22.9,
+      "childPoverty": 9.1,
+      "rentBurden": 48.8,
+      "uninsured": 4.2
+    },
+    "51": {
+      "name": "Virginia",
+      "olderAdults": 17.6,
+      "childPoverty": 11.7,
+      "rentBurden": 49.3,
+      "uninsured": 6.9
+    },
+    "53": {
+      "name": "Washington",
+      "olderAdults": 17.3,
+      "childPoverty": 11.4,
+      "rentBurden": 50.7,
+      "uninsured": 6.5
+    },
+    "54": {
+      "name": "West Virginia",
+      "olderAdults": 21.9,
+      "childPoverty": 21.6,
+      "rentBurden": 47,
+      "uninsured": 5.8
+    },
+    "55": {
+      "name": "Wisconsin",
+      "olderAdults": 19.6,
+      "childPoverty": 12.2,
+      "rentBurden": 44.9,
+      "uninsured": 5.3
+    },
+    "56": {
+      "name": "Wyoming",
+      "olderAdults": 20,
+      "childPoverty": 9.5,
+      "rentBurden": 42.9,
+      "uninsured": 10.3
+    },
+    "US": {
+      "name": "United States",
+      "olderAdults": 18,
+      "childPoverty": 15.5,
+      "rentBurden": 51.8,
+      "uninsured": 8.2
+    },
+    "01": {
+      "name": "Alabama",
+      "olderAdults": 18.5,
+      "childPoverty": 20.2,
+      "rentBurden": 48.2,
+      "uninsured": 8.2
+    },
+    "02": {
+      "name": "Alaska",
+      "olderAdults": 14.8,
+      "childPoverty": 13,
+      "rentBurden": 41.5,
+      "uninsured": 11
+    },
+    "04": {
+      "name": "Arizona",
+      "olderAdults": 19.7,
+      "childPoverty": 14.6,
+      "rentBurden": 52.4,
+      "uninsured": 10.3
+    },
+    "05": {
+      "name": "Arkansas",
+      "olderAdults": 18.2,
+      "childPoverty": 20,
+      "rentBurden": 45.5,
+      "uninsured": 9.4
+    },
+    "06": {
+      "name": "California",
+      "olderAdults": 16.5,
+      "childPoverty": 14.6,
+      "rentBurden": 55.8,
+      "uninsured": 5.9
+    },
+    "08": {
+      "name": "Colorado",
+      "olderAdults": 16.5,
+      "childPoverty": 11.5,
+      "rentBurden": 52.5,
+      "uninsured": 7.9
+    },
+    "09": {
+      "name": "Connecticut",
+      "olderAdults": 19.4,
+      "childPoverty": 13.4,
+      "rentBurden": 54.1,
+      "uninsured": 5.8
+    }
+  }
+};
